e44a392a1b05af6fe6698dab7ad6b61ce61e272b
Riadh <gharbiriadh48@gmail.com>
2021-10-30 15:44:08 +0100

    init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,8 +15,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2490dab5a1b5c2b4ae718be7634caa2b",
-"/": "2490dab5a1b5c2b4ae718be7634caa2b",
+"index.html": "af51d783579aae387a404999a714ba2e",
+"/": "af51d783579aae387a404999a714ba2e",
 "main.dart.js": "2d305499a5cc92e34bba67f96519255e",
 "manifest.json": "3583716044496e452e6f5d01d922d0bd",
 "version.json": "ad2c14e085a968f74482a37f2ce043a9"
